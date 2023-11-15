1f0433051e1e1e0388502c390b1b9ed9e6cef7fc
2022345 <2022345@student.cct.ie>
2023-11-15 13:38:20 +0000

    Update HospitalAdministrationn.java
    created code

--- src/hospitaladministrationn/HospitalAdministrationn.java
+++ src/hospitaladministrationn/HospitalAdministrationn.java
@@ -15,6 +15,27 @@ public class HospitalAdministrationn {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Patient p1 = new Patient ("Sam", "15/11/23", "O+");
+        Patient p2 = new Patient ("Lexie", "27/11/21", "A-");
+        
+        System.out.println(p1.getPatientID());
+        System.out.println(p2.getPatientID());
+        
+        System.out.println(Patient.getCurrentID());
+       if  (DatabaseSetup.setupDB){
+           System.out.println("Database and table created");
+       }else{
+           System.out.println("On no! There was a database creation problem...");
+       }
+        
     }
     
 }
