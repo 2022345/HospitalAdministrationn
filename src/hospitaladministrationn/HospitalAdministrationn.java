e93dc6e69647d433536ef21479e20ffe252bc2f8
2022345 <2022345@student.cct.ie>
2023-11-15 14:33:01 +0000

    Update HospitalAdministrationn.java
    added

--- src/hospitaladministrationn/HospitalAdministrationn.java
+++ src/hospitaladministrationn/HospitalAdministrationn.java
@@ -4,6 +4,10 @@
  */
 package hospitaladministrationn;
 
+import java.sql.SQLException;
+
+
+
 /**
  *
  * @author suele
@@ -12,30 +16,36 @@ public class HospitalAdministrationn {
 
     /**
      * @param args the command line arguments
+     * @throws java.lang.ClassNotFoundException
+     * @throws java.lang.InstantiationException
+     * @throws java.lang.IllegalAccessException
+     * @throws java.sql.SQLException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
         // TODO code application logic here
         
+     
         
-        /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        Patient p1 = new Patient ("Sam", "15/11/23", "O+");
-        Patient p2 = new Patient ("Lexie", "27/11/21", "A-");
+        Patient p1 = new Patient ("Sam", "2023-11-15", "O+");
+        Patient p2 = new Patient ("Lexie", "2021-11-27", "A-");
         
         System.out.println(p1.getPatientID());
         System.out.println(p2.getPatientID());
         
         System.out.println(Patient.getCurrentID());
-       if  (DatabaseSetup.setupDB){
+        
+       if  (DatabaseSetup.setupDB()){
            System.out.println("Database and table created");
        }else{
            System.out.println("On no! There was a database creation problem...");
        }
+       
+       DatabaseWriter dbw = new DatabaseWriter();
+       if (dbw.addPatient (p1)){
+           System.out.println("p1 added");
+       }
         
     }
-    
 }
+    
+
